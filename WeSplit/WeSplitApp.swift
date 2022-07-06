3de8cae2ed9a66fa8144fbb21681926af3d85a72
antieatingactivist <antieatingactivist@gmail.com>
2022-07-05 22:19:49 -0700

    initial commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Garrett Corbin on 7/5/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
